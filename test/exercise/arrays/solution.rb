bf5eb1cfc59460c8ffe7fbcdb334dd14a1584409
Sergey Heinrich <hans.nordwand@gmail.com>
2021-11-18 14:21:12 +0300

    ver 2 (#2 added recursion, #1 without using standard functions .max\.map)

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,23 +2,36 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map { |i| i.positive? ? array.max : i }
+        max = array[0]
+        array.size.times do
+          array.rotate!
+          max = array[0] if array[0] > max
+        end
+
+        i = 0
+        while i < array.size
+          array[i] = array[i].positive? ? max : array[i]
+          i += 1
+        end
+
+        array
       end
 
-      def search(arr, qry)
-        idx_start = 0
-        idx_end = arr.length
-        loop do
-          len = idx_end - idx_start
-          return arr[idx_start] == qry ? idx_start : -1 if len < 2
+      def search(arr, qry, idx_start = nil, idx_end = nil)
+        idx_start ||= 0
+        idx_end ||= arr.length
 
-          idx_cen = len / 2 + idx_start
-          if arr[idx_cen] > qry
-            idx_end = idx_cen
-          else
-            idx_start = idx_cen
-          end
+        len = idx_end - idx_start
+        return arr[idx_start] == qry ? idx_start : -1 if len < 2
+
+        idx_cen = len / 2 + idx_start
+        if arr[idx_cen] > qry
+          idx_end = idx_cen
+        else
+          idx_start = idx_cen
         end
+
+        search(arr, qry, idx_start, idx_end)
       end
     end
   end
