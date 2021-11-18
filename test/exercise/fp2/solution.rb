a2a43e1582b0a698b7095eff7bf0caaca061ecfd
Sergey Heinrich <hans.nordwand@gmail.com>
2021-11-18 15:49:16 +0300

    Solving problems fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 module Exercise
   module Fp2
     class MyArray < Array
@@ -5,16 +7,50 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(items = nil, &block)
+        items ||= self
+        one, *items = items
+
+        yield(one)
+        my_each(items, &block) unless items.blank?
+
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        @out_ary = Exercise::Fp2::MyArray.new
+
+        my_each(self) do |item|
+          @out_ary << yield(item)
+        end
+
+        @out_ary
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        @new_ary = Exercise::Fp2::MyArray.new
+
+        my_each(self) do |item|
+          value = !block_given? ? item : yield(item)
+          @new_ary << value unless value.nil?
+        end
+
+        @new_ary
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, items = nil, &block)
+        items ||= dup
+        acc ||= items.delete_at(0)
+
+        acc = yield(acc, items.delete_at(0))
+
+        return acc if items.blank?
+
+        my_reduce(acc, items, &block)
+      end
     end
   end
 end
